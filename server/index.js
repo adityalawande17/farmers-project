6fe1569ef0b6c9af35a3c72570ca156c2ec67f56
aditya <adityalawande17@gmail.com>
2026-05-09 01:17:27 +0530

    Fixed vercel error

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
     credentials: true,
   }),
 );
